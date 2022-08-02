3fa0b0b16adb5f5afb26a180ecb33ec1625061ed
Steven-Dev <107848175+Stv-devl@users.noreply.github.com>
2022-08-02 22:13:09 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+// variable
+const form = document.querySelector("form");
+
+// Function for storage part
+function storeList() {
+  window.localStorage.todoList = list.innerHTML;
+}
+
+function getTodos() {
+  if (window.localStorage.todoList) {
+    list.innerHTML = window.localStorage.todoList;
+  } else {
+    list.innerHTML = `<li>Cliquez sur un todo pour le supprimer</li>`;
+  }
+}
+
+window.addEventListener("load", getTodos);
+
+// Add element
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  list.innerHTML += `<li>${item.value}</li>`;
+  item.value = "";
+  storeList();
+});
+
+// Remove element
+list.addEventListener("click", (e) => {
+  if (e.target.classList.contains("checked")) {
+    e.target.remove();
+  } else {
+    e.target.classList.add("checked");
+  }
+  storeList();
+});
