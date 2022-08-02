c843b307975dbc7c1e2209c332b2afba261d3303
Steven-Dev <107848175+Stv-devl@users.noreply.github.com>
2022-08-02 17:10:58 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -6,28 +6,26 @@
 const form = document.querySelector("form");
 const body = document.querySelector("body");
 const input = document.querySelector("input");
-const todo = document.querySelector(".todo1");
-const span = document.querySelector("span");
-
-/*
-const inputChecker = (value) => {
-  if (value.length > 0 && (value.length < 3 || value.length > 80)) {
-    span.innerHTML = `You have to write something`;
-    toDo = null;
-  } else {
-    toDo = value;
-  }
-};*/
+const todo = document.querySelector(".todolist");
 
 document.addEventListener("keypress", (e) => {
-  /* console.log(input.value);*/
   if (e.key == "Enter") {
-    todo.innerHTML = `<p><i class="fa-solid fa-star-of-life"></i>${input.value}<i id="delete"m class="fa-solid fa-trash"></i></p>`;
+    //create div
+    const todoDiv = document.createElement("div");
+    todoDiv.classList.add("todo-div");
+    //create Li + button
+    const newTodo = document.createElement("li");
+    newTodo.innerHTML = `<p><i class="fa-solid fa-star-of-life"></i>${input.value}<i id="delete"m class="fa-solid fa-trash"></i></p>`;
+    newTodo.classList.add("todo-item");
+    todoDiv.appendChild(newTodo);
+    //add to list
+    todo.appendChild(todoDiv);
+    //delete div on click
+    todoDiv.addEventListener("click", () => {
+      todoDiv.remove();
+    });
+    //prevent default and empty the form
     e.preventDefault();
+    input.value = "";
   }
-});
-
-// when we click on todo html will be remove
-todo.addEventListener("click", () => {
-  todo.innerHTML = ``;
 });
