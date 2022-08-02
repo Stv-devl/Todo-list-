bdc4994e6e6f1fa03326e2d5da1422d6d8522f16
Steven-Dev <107848175+Stv-devl@users.noreply.github.com>
2022-08-02 10:32:26 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -4,8 +4,32 @@
 //stoker dans le local storage de la liste
 // consulter le local storage au lancement de l'app pour rajouter des todos
 const form = document.querySelector("form");
+const body = document.querySelector("body");
 const input = document.querySelector("input");
+const todo = document.querySelector(".todo1");
+const span = document.querySelector("span");
 
-input.addEventListener("input", (e) => {
-  console.log(e.target.value);
+/*
+const inputChecker = (value) => {
+  if (value.length > 0 && (value.length < 3 || value.length > 80)) {
+    span.innerHTML = `You have to write something`;
+    toDo = null;
+  } else {
+    toDo = value;
+  }
+};*/
+
+document.addEventListener("keypress", (e) => {
+  console.log(input.value);
+  if (e.key == "Enter") {
+    todo.innerHTML = `${input.value}`;
+    e.preventDefault();
+  }
 });
+
+/*
+document.addEventListener("input", (e) => {
+  input.value == todo;
+  document.todo.innerHTML = "todo";
+});
+*/
